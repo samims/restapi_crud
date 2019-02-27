7b5033f4c4189f0b821980c444ffd9bdbf0dfb6a
DAT-Asset-164 <>
2019-02-27 18:02:53 +0530

    token obtain working

---
+++ blogs/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=30, null=True)
+    content = models.TextField(blank=False, null=True)
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    title = models.CharField(max_length=100, blank=False, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True, blank=False, null=True)
+
+    objects = CustomUserManager
+
+    def __str__(self):
+        return self.email
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
